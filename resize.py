84d0bc6b9a4d8985867a2a48774cb31b3492e63f
Harmony <sd.harmony@yahoo.fr>
2022-01-12 11:34:29 +0100

    creations des neg en gris + d'autres positives a partir de negatif

--- resize.py
+++ resize.py
@@ -11,13 +11,16 @@ def resize(nom):
         filename = "images/" + nom + ".jpg"  
         #lire l'image
         oriimg = cv2.imread(filename) 
+        img = cv2.imread(filename,cv2.IMREAD_GRAYSCALE)
         # redimensionnement de l'image
-        newimg = cv2.resize(oriimg,(50, 50))  
+        newimg = cv2.resize(img,(100, 100))  
         # afficher l'image
-        cv2.imshow("Show by CV2",newimg)
-        cv2.waitKey(0)
+        #cv2.imshow("Show by CV2",newimg)
+        #cv2.waitKey(0)
+        if not os.path.exists('neg'):
+            os.makedirs('neg')
         # sauvegarder l'image
-        cv2.imwrite(nom + ".jpg",newimg)
+        cv2.imwrite("neg/"+nom + ".jpg",newimg)
         #importation de librairie nécessaires 
 
 
@@ -26,4 +29,25 @@ def resize(nom):
     except Exception as e:
         print(str(e))
 
+resize("cerf")
+resize("chevre")
+resize("chien")
+resize("elephant")
+resize("faucon")
+resize("gnou")
+resize("grizzly")
+resize("guepard")
+resize("hamster")
+resize("lama")
+resize("lion")
 resize("marmotte")
+resize("panda")
+resize("poisson")
+resize("racoon")
+resize("renard")
+resize("serpent")
+resize("singe")
+resize("singe")
+resize("taureau")
+resize("zebra")
+resize("zemmour")
