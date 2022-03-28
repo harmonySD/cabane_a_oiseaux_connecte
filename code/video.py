146d23220366c7c5c6b1e71d77922c0fa6a4bb87
Harmony <sd.harmony@yahoo.fr>
2022-03-28 14:11:39 +0200

    essaie de prendre de nouvelles photo du fond toutes les 10, sec

--- code/video.py
+++ code/video.py
@@ -1,10 +1,21 @@
-
+import cv2
+import time
 from mask import create_mask
 cap=cv2.VideoCapture(0)
-image_fond = cv2.imread(
-    "info_image_oiseaux/image_blanche.jpeg")
+#image_fond = cv2.imread(
+ #   "info_image_oiseaux/image_blanche.jpeg")
+time.sleep(5)
 
-while True:  
+def new_fond():
+    ret, image_fond=cap.read()#mask erode mask dilate dans create_mask pour faire moins precie et gere la
+#variation de lumiere 
+    cv2.imshow("fond",image_fond)
+    return image_fond
+
+#pour cqhanger de fond toute les 10 minutes
+image_fond=new_fond()
+begin_time= time.localtime(time.time())
+while True: 
     ret, frame=cap.read()
     mask=create_mask(frame,image_fond,10)
     cv2.imshow("mask",mask)
@@ -14,6 +25,15 @@ while True:
     if cv2.waitKey(1)&0xFF==ord('p'):
         ret, pic=cap.read()
         cv2.imshow("pic",pic)
+     #pour changer de fond
+    now= time.localtime(time.time())
+   
+    if(int(time.strftime("%S",begin_time))+10<int(time.strftime("%S",now))):
+        image_fond=new_fond
+        print(time.strftime("%S",begin_time))
+        print(time.strftime("%S",now))
+        begin_time=time.localtime(time.time())
+    
 cap.release()
 cv2.destroyAllWindows()
 
