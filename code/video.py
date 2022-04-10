1668793b99e702f74333c99451ee3191390ed7c9
Harmony <sd.harmony@yahoo.fr>
2022-04-10 18:26:15 +0200

    essai seuil

--- code/video.py
+++ code/video.py
@@ -47,7 +47,7 @@ score = []
 while True: 
     ret, frame=cap.read()
 
-    mask=create_mask(frame,image_fond,30)
+    mask=create_mask(frame,image_fond,20)
     
     cv2.imshow("mask",mask)
     #cv2.imshow('Camera', frame)
